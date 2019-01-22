1791196f01e984435c770a5bf7512394eb7e10e8
repeto1204 <45305363+repeto1204@users.noreply.github.com>
2019-01-22 16:59:41 +0900

    author : JJH | message : header background 수정, img 경로 수정, js 주석처리, faq content font-size 수정

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const header = document.querySelector('header');
+const header_bg = document.querySelector('.header_bg');
 const banner = document.getElementById('banner');
 const header_height = document.querySelector('.header_inner').clientHeight;
 var banner_height=0;
@@ -14,42 +15,39 @@ const arrow_r = document.querySelector('.arrow_r');
 const pre_box = document.querySelector('.prev_box');
 const cur_box = document.querySelector('.cur_box');
 const next_box = document.querySelector('.next_box');
-var cur_box_flag = 3;
+var cur_box_flag = 3; // preview box에 표시될 img 갯수
 
 
 const faq_btn = document.querySelectorAll(".faq_btn");
 const faqs_content = document.querySelectorAll('.faqs_content');
 var fqas_index = 0;
 
+
+
+
+
+// scroll 이동시 nav background-color 변경
+window.addEventListener('scroll', function(){
+    banner_height = banner.clientHeight; // banner 높이값 get
+    change_nav(banner_height);
+});
+
+
+
+
+
+
+// fqas content box display 초기화
 faqs_content.forEach(l => {
     l.style.display="none";
 });
 
 
 
-faq_btn.forEach(e => {
-    e.addEventListener('click',function(){
-        faqs_index = Array.from(faq_btn).indexOf(this);
-        
-        faqs_content.forEach((val, index, l) => {
-            if(faqs_index!=index){
-                l[index].style.display="none";
-            }
-        });
-        if(faqs_content[faqs_index].style.display=='none'){
-            faqs_content[faqs_index].style.display='block';
-        } else if(faqs_content[faqs_index].style.display=='block'){
-            faqs_content[faqs_index].style.display='none';
-        }
-    });
-});
-
-faqs_content.forEach(e => {
-    e.style.display="none";
-});
 
 
 
+// menu 버튼 클릭시 숨김 및 활성화
 side_btn1.addEventListener('click',function(){;
     side_menu.style.marginRight = 0;
     setTimeout(change_menu_flag, 1);
@@ -61,6 +59,7 @@ side_btn2.addEventListener('click',function(){;
 });
 
 
+// menu 활성화 상태에서 다른 영역 클릭 및 메뉴 이동시 메뉴 비활성화
 window.addEventListener('click',function(){
     if(menu_flag){
         side_menu.style.marginRight = -160+'px';
@@ -69,14 +68,12 @@ window.addEventListener('click',function(){
 });
 
 
-// scroll 이동시 nav background-color 변경
-window.addEventListener('scroll', function(){
-    banner_height = banner.clientHeight; // banner 높이값 get
-    change_nav(banner_height);
-});
 
 
 
+
+
+//화살표 버튼 클릭시 preview img 이동
 arrow_r.addEventListener('click',function(){
     cng_pic_r();
 });
@@ -86,6 +83,48 @@ arrow_l.addEventListener('click',function(){
 });
 
 
+
+
+
+
+
+// faq btn click시 해당 index와 같은 faq content 활성화
+faq_btn.forEach(e => {
+    e.addEventListener('click',function(){
+        faqs_index = Array.from(faq_btn).indexOf(this);
+        
+        faqs_content.forEach((val, index, l) => {
+            if(faqs_index!=index){
+                l[index].style.display="none";
+            }
+        });
+
+        // 이미 활성화 상태이면 비활성화
+        if(faqs_content[faqs_index].style.display=='none'){
+            faqs_content[faqs_index].style.display='block';
+        } else if(faqs_content[faqs_index].style.display=='block'){
+            faqs_content[faqs_index].style.display='none';
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ * @brief arrow_r button click시 preview img 이동
+ * @author JJH
+ * @see . img 갯수가 증가하면 cur_box_flag와 내부 함수를 수정해아한다.
+ * @todo img 증가시 함수 수정
+ * @deprecated 삭제예정
+ * 
+ */
 function cng_pic_r() {
     cur_box_flag++;
     if(cur_box_flag>3){
@@ -115,6 +154,19 @@ function cng_pic_r() {
     }
 }
 
+
+
+
+
+/**
+ * 
+ * @brief arrow_l button click시 preview img 이동
+ * @author JJH
+ * @see . img 갯수가 증가하면 cur_box_flag와 내부 함수를 수정해아한다.
+ * @todo img 증가시 함수 수정
+ * @deprecated 삭제예정
+ * 
+ */
 function cng_pic_l() {
     cur_box_flag--;
     if(cur_box_flag<1){
@@ -146,7 +198,13 @@ function cng_pic_l() {
 
 
 
-
+/**
+ * 
+ * @brief side_menu 제어를 위해 settimeout에 들어가는 함수
+ * @author JJH
+ * @see . window click시 비활성화를 위한 menu flag 변경 함수, settimeout과 함께 써야함
+ * 
+ */
 function change_menu_flag() {
     menu_flag = 1;
 }
@@ -163,9 +221,13 @@ function change_menu_flag() {
  * 
  */
 function change_nav(height) {
-    if(window.scrollY<height-header_height){
+    if(window.scrollY<10){
+        header_bg.style.display="none"
+    }else if(window.scrollY<height-header_height){
         header.style.background = 'none';
+        header_bg.style.display="block"
     } else if (window.scrollY>height-header_height){
+        header_bg.style.display="none"
         header.style.backgroundColor = '#532480';
     }
 }
