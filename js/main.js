5d89a6037400734b3ea18c513989b86ae53b37e3
repeto1204 <45305363+repeto1204@users.noreply.github.com>
2019-01-22 15:49:38 +0900

    author : JJH | message : window click시 side menu hide /  preview box, faqs box javascript 적용

--- js/main.js
+++ js/main.js
@@ -1,21 +1,72 @@
-var header = document.querySelector('header');
-var banner = document.getElementById('banner');
+const header = document.querySelector('header');
+const banner = document.getElementById('banner');
+const header_height = document.querySelector('.header_inner').clientHeight;
 var banner_height=0;
 
-var side_menu = document.querySelector('.side_menu');
-var side_btn1 = document.querySelector('.side_btn1');
-var side_btn2 = document.querySelector('.side_btn2');
+const side_menu = document.querySelector('.side_menu');
+const side_btn1 = document.querySelector('.side_btn1');
+const side_btn2 = document.querySelector('.side_btn2');
+var menu_flag = 0;
 
-side_btn1.addEventListener('click',function(){
+
+const arrow_l = document.querySelector('.arrow_l');
+const arrow_r = document.querySelector('.arrow_r');
+const pre_box = document.querySelector('.prev_box');
+const cur_box = document.querySelector('.cur_box');
+const next_box = document.querySelector('.next_box');
+var cur_box_flag = 3;
+
+
+const faq_btn = document.querySelectorAll(".faq_btn");
+const faqs_content = document.querySelectorAll('.faqs_content');
+var fqas_index = 0;
+
+faqs_content.forEach(l => {
+    l.style.display="none";
+});
+
+
+
+faq_btn.forEach(e => {
+    e.addEventListener('click',function(){
+        faqs_index = Array.from(faq_btn).indexOf(this);
+        
+        faqs_content.forEach((val, index, l) => {
+            if(faqs_index!=index){
+                l[index].style.display="none";
+            }
+        });
+        if(faqs_content[faqs_index].style.display=='none'){
+            faqs_content[faqs_index].style.display='block';
+        } else if(faqs_content[faqs_index].style.display=='block'){
+            faqs_content[faqs_index].style.display='none';
+        }
+    });
+});
+
+faqs_content.forEach(e => {
+    e.style.display="none";
+});
+
+
+
+side_btn1.addEventListener('click',function(){;
     side_menu.style.marginRight = 0;
+    setTimeout(change_menu_flag, 1);
 });
 
-side_btn2.addEventListener('click',function(){
+side_btn2.addEventListener('click',function(){;
     side_menu.style.marginRight = -160+'px';
+    menu_flag=0;
 });
 
 
-
+window.addEventListener('click',function(){
+    if(menu_flag){
+        side_menu.style.marginRight = -160+'px';
+        menu_flag=0;
+    }
+});
 
 
 // scroll 이동시 nav background-color 변경
@@ -26,15 +77,79 @@ window.addEventListener('scroll', function(){
 
 
 
+arrow_r.addEventListener('click',function(){
+    cng_pic_r();
+});
+
+arrow_l.addEventListener('click',function(){
+    cng_pic_l();
+});
+
+
+function cng_pic_r() {
+    cur_box_flag++;
+    if(cur_box_flag>3){
+        cur_box_flag=1;
+    }
+    switch (cur_box_flag) {
+        case 1:
+            pre_box.setAttribute('src','image/preview'+cur_box_flag+'.png');
+            cur_box.setAttribute('src','image/preview'+(cur_box_flag+1)+'.png');
+            next_box.setAttribute('src','image/preview'+(cur_box_flag+2)+'.png');
+            break;
+
+        case 2:
+            pre_box.setAttribute('src','image/preview'+cur_box_flag+'.png');
+            cur_box.setAttribute('src','image/preview'+(cur_box_flag+1)+'.png');
+            next_box.setAttribute('src','image/preview'+(cur_box_flag-1)+'.png');
+            break;
+
+        case 3:
+            pre_box.setAttribute('src','image/preview'+cur_box_flag+'.png');
+            cur_box.setAttribute('src','image/preview'+(cur_box_flag-2)+'.png');
+            next_box.setAttribute('src','image/preview'+(cur_box_flag-1)+'.png');
+            cur_box_flag=0;
+            break;
+        default:
+            break;
+    }
+}
+
+function cng_pic_l() {
+    cur_box_flag--;
+    if(cur_box_flag<1){
+        cur_box_flag=3;
+    }
+    switch (cur_box_flag) {
+        case 1:
+            pre_box.setAttribute('src','image/preview'+cur_box_flag+'.png');
+            cur_box.setAttribute('src','image/preview'+(cur_box_flag+1)+'.png');
+            next_box.setAttribute('src','image/preview'+(cur_box_flag+2)+'.png');
+            cur_box_flag=4;
+            break;
+
+        case 2:
+            pre_box.setAttribute('src','image/preview'+cur_box_flag+'.png');
+            cur_box.setAttribute('src','image/preview'+(cur_box_flag+1)+'.png');
+            next_box.setAttribute('src','image/preview'+(cur_box_flag-1)+'.png');
+            break;
+
+        case 3:
+            pre_box.setAttribute('src','image/preview'+cur_box_flag+'.png');
+            cur_box.setAttribute('src','image/preview'+(cur_box_flag-2)+'.png');
+            next_box.setAttribute('src','image/preview'+(cur_box_flag-1)+'.png');
+            break;
+        default:
+            break;
+    }
+}
 
 
 
 
-
-
-
-
-
+function change_menu_flag() {
+    menu_flag = 1;
+}
 
 
 
@@ -48,9 +163,9 @@ window.addEventListener('scroll', function(){
  * 
  */
 function change_nav(height) {
-    if(window.scrollY<height){
+    if(window.scrollY<height-header_height){
         header.style.background = 'none';
-    } else if (window.scrollY>height){
+    } else if (window.scrollY>height-header_height){
         header.style.backgroundColor = '#532480';
     }
 }
